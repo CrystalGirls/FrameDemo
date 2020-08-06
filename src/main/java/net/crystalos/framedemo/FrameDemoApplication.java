612d38a358e2c89aaccc7c04d393ae68dfc63079
Crystal <crystal@crystalos.net>
2020-08-06 17:55:28 +0800

    加入可能会引起异常字符的处理，先注释掉

--- src/main/java/net/crystalos/framedemo/FrameDemoApplication.java
+++ src/main/java/net/crystalos/framedemo/FrameDemoApplication.java
@@ -2,6 +2,10 @@ package net.crystalos.framedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FrameDemoApplication {
@@ -9,5 +13,11 @@ public class FrameDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(FrameDemoApplication.class, args);
     }
-
+/*
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setProperty("relaxedQueryChars", "|{}[]"));
+        return factory;
+    }*/
 }
